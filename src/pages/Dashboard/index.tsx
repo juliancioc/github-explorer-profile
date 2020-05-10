c3a88759d41129ddc2857ec22e9ecffd5a03cbbe
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-05-10 09:00:06 -0300

    add repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg'
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
     full_name: string;
@@ -16,19 +17,47 @@ interface Repository {
 }
 
 const Dashboard: React.FC = () => {
-    const [newRepo, setNewRepo] = useState('')
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories)
+        );
+    }, [repositories])
 
     const handleAddedRepository = async (event: FormEvent<HTMLFormElement>,
     ): Promise<void> => {
         event.preventDefault();
 
-        const response = await api.get<Repository>(`repos/${newRepo}`);
+        if (!newRepo) {
+            setInputError('Digite o auto/nome do repositório');
+            return;
+        }
 
-        const repository = response.data;
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
 
-        setRepositories([...repositories, repository]);
-        setNewRepo('')
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Error na busca por esse repositório');
+        }
+
     }
 
     return (
@@ -36,7 +65,7 @@ const Dashboard: React.FC = () => {
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form onSubmit={handleAddedRepository}>
+            <Form hasError={!!inputError} onSubmit={handleAddedRepository}>
                 <input
                     placeholder="Digite o nome do repositório"
                     value={newRepo}
@@ -45,9 +74,11 @@ const Dashboard: React.FC = () => {
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {inputError && <Error>{inputError}</Error>}
+
             <Repositories>
                 {repositories.map((repository) => (
-                    <a key={repository.full_name} href="#">
+                    <Link key={repository.full_name} to={`repositories/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -58,7 +89,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>
-}
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/13937762?s=460&u=0a50919caf2890ec2208a513377c5412bf1d7bac&v=4" alt="Juliancio" />
+                    <div>
+                        <strong>juliancio/portfolio</strong>
+                        <p>descricao do repositorio</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Starts</span>
+                    </li>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="#">
+                    <div>
+                        <strong>fsdffsfd</strong>
+                        <p>fdsfdssdffsdf</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
+};
 
 export default Repository;
